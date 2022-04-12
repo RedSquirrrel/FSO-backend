fc87c09e1b9bc53a2dd4d9f6e5552ed1cedf24d7
RedSquirrrel <anet1@rocketmail.com>
2022-04-12 16:49:16 +0100

    ex: 3.1-3.6

---
+++ 2022/phonebook-backend-part3/index.js
@@ -0,0 +1,91 @@
+const { response } = require('express');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+let persons = [
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040-123456',
+  },
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+  },
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+  },
+  {
+    id: 4,
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+  },
+];
+
+const generateID = () => {
+  const id = Math.floor(Math.random() * 1000000);
+  return id;
+};
+
+app.get('/info', (request, response) => {
+  response.send(`<div>Phonebook has info for ${persons.length} people </div> <div>${new Date()}</div>`);
+});
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons);
+});
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id;
+  const person = persons.find(p => p.id === Number(id));
+  if (person) {
+    response.json(person);
+  } else {
+    return response.status(404).end();
+  }
+});
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id;
+  persons = persons.filter(p => p.id !== Number(id));
+
+  response.status(204).end();
+});
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'Missing name or number',
+    });
+  }
+
+  const findPerson = persons.map(p => p.name.toLowerCase() === body.name.toLowerCase());
+
+  if (findPerson.find(p => p === true)) {
+    return response.status(400).json({
+      error: 'Name must be unique',
+    });
+  }
+
+  const person = {
+    id: generateID(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # FSO-backend
+
+[2021](https://github.com/RedSquirrrel/phonebook-backend-part3-2021)
+
+[2022](https://github.com/RedSquirrrel/FSO-backend)
