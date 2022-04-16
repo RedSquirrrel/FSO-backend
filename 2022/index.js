4a5304b0314e79e76b11beeeefecba3654999d90
RedSquirrrel <anet1@rocketmail.com>
2022-04-16 13:52:11 +0100

    ex: 3.22

--- 2022/index.js
+++ 2022/index.js
@@ -54,6 +54,7 @@ app.delete('/api/persons/:id', (request, response) => {
 });
 
 morgan.token('body', function (req, res) {
+  console.log(res);
   return JSON.stringify(req.body);
 });
 app.use(morgan(':method :url :status  :res[content-length] - :response-time ms :body'));
